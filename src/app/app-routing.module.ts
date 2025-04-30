6dd904f283db90a1d0ed3b038fec1cb7ab2db027
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-04-30 10:39:25 +0100

    la creation les composant avec leas route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
+import {RatingSystemComponent} from './rating-system/rating-system.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "login", component : LoginComponent},
+  {path : "home" , component : HomeComponent},
+  {path : "recipe-list", component : RecipeListComponent},
+  {path : "recipe-detail", component : RecipeDetailComponent},
+  {path : "rating", component : RatingSystemComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { LoginComponent } from './login/login.component';
+import { RatingSystemComponent } from './rating-system/rating-system.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    LoginComponent,
+    RatingSystemComponent
   ],
   imports: [
     BrowserModule,
